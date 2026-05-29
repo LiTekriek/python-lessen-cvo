daba8cb5b2fd98a8aef0c7465f41105c53725668
LiTekriek <83124146+LiTekriek@users.noreply.github.com>
2026-05-29 10:18:49 +0200

    OefB klaar

---
+++ Les2_OefB.py
@@ -0,0 +1,5 @@
+naam = input("Geef je voornaam in:")
+leeftijd = int(input("geef je leeftijd in: "))
+
+print(f"Hallo {naam}! Volgend jaar ben je {leeftijd + 1} jaar oud.")
+print(naam.upper())
---
+++ les2_oefA.py
@@ -0,0 +1,9 @@
+voornaam = "Rios"
+leeftijd = 18
+fav_getal = 6
+print(voornaam)
+print(leeftijd)
+print(fav_getal)
+print(type(voornaam))
+print(type(leeftijd))
+print(type(fav_getal))
