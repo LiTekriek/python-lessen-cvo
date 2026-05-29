670cd881c59292a81af3d06074dbda1854e65b74
LiTekriek <83124146+LiTekriek@users.noreply.github.com>
2026-05-29 10:26:56 +0200

    oefC deel 2 klaar

--- Les2_oefC.py
+++ Les2_oefC.py
@@ -5,3 +5,10 @@ print("Som", getal1 + getal2)
 print("Verschil", getal1 - getal2)
 print("Product", getal1 * getal2)
 print("Deling", getal1 / getal2)
+
+if getal1 % 2 == 0:
+    print("Het getal is even")
+else:
+    print("Het getal is oneven")
+
+print("Kwadraat", getal1 ** 2)
