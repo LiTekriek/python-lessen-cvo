396e716ffbad52e365293775444b9c32e5cbd3f4
LiTekriek <83124146+LiTekriek@users.noreply.github.com>
2026-05-29 10:19:27 +0200

    OefC Klaar

---
+++ Les2_oefC.py
@@ -0,0 +1,7 @@
+getal1 = int(input("Geef het eerste getal in: "))
+getal2 = int(input("Geef het tweede getal in: "))
+
+print("Som", getal1 + getal2)
+print("Verschil", getal1 - getal2)
+print("Product", getal1 * getal2)
+print("Deling", getal1 / getal2)
